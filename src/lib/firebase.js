cfdc304d087fec54723e615954c9720a90ce3dbc
code-Kawhe <142457225+code-Kawhe@users.noreply.github.com>
2025-08-12 22:02:38 -0300

    Update firebase.js

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -4,14 +4,14 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDY6JpGcG1VqCWi5LfPnyl4kOMVcdm7RmM",
-    authDomain: "kawhe-alves-dos-santos-db4be.firebaseapp.com",
-    databaseURL: "https://kawhe-alves-dos-santos-db4be-default-rtdb.firebaseio.com",
-    projectId: "kawhe-alves-dos-santos-db4be",
-    storageBucket: "kawhe-alves-dos-santos-db4be.appspot.com",
-    messagingSenderId: "1074140345530",
-    appId: "1:1074140345530:web:926dc6276af8fc9dfa7e83",
-    measurementId: "G-9JNBLHFMRR"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
